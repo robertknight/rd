e52d8a36e45905a415fe4c0610d607df82d26f92
Robert Knight <robertknight@gmail.com>
2013-11-25 19:20:16 +0000

    Document cdr command and support multiple pattern args
    cdr now supports multiple pattern arguments, these are all
    passed directly to rd.
    
    This means that 'cdr foo bar' is now supported where the dir
    path must contain both 'foo' and 'bar'

--- rd.bash
+++ rd.bash
@@ -1,5 +1,28 @@
+#!/usr/bin/env bash
+
+# A set of utility functions which integrate
+# rd with bash.
+#
+# Add 'source <path>/rd.bash' to ~/.bashrc
+# to make these facilities available in new shells.
+
+# cdr - Switch to a recently used directory.
+#
+# Usage:
+#   cdr <pattern>
+#   cdr <id>
+#
+# The first form searches for a recently used dir matching a given
+# pattern. If there is a single result, cdr switches
+# to the given dir. If there are multiple matches,
+# prints a list of matches alongside a numeric ID for
+# each.
+#
+# The second form 'cdr <id>' can then be used to switch
+# to a dir listed by a recent 'cdr <pattern>' query.
+#
 function cdr {
-	matches=`rd $1`
+	matches=`rd $@`
 
 	# Check whether the response was a single match
 	# or a list of possible matches
