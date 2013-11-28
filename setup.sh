3bcff5b2ef706dea740b16efb670278439cf613b
Robert Knight <robertknight@gmail.com>
2013-11-28 18:23:32 +0000

    Add logic to run daemon on login on OS X and setup Bash integration
    A launchd agent is used to run the daemon on login under OS X
    
    Also remove existing references to rd.bash from
    the shell's init file (if any)

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+if [ `uname` == 'Darwin' ]
+then
+	OSX=1
+fi
+
 echo "Building 'rd'..."
 
 # Build the 'rd' tool
@@ -23,16 +28,40 @@ echo "Setting up daemon autostart and shell integration"
 RD_BIN_PATH=$PWD/rd
 
 # Autostart the rd daemon at login
-AUTOSTART_DIR=~/.config/autostart
-cp -f $PWD/rd-autostart.desktop $AUTOSTART_DIR/
-echo "Exec=${RD_BIN_PATH} -daemon" >> $AUTOSTART_DIR/rd-autostart.desktop
+if [ $OSX ]
+then
+	LAUNCHD_AGENT_FILENAME=com.github.robertknight.rd.plist
+	sed "s:\$RD_BIN_PATH:$RD_BIN_PATH:" $LAUNCHD_AGENT_FILENAME > ~/Library/LaunchAgents/$LAUNCHD_AGENT_FILENAME
+else
+	AUTOSTART_DIR=~/.config/autostart
+	if [ -d $AUTOSTART_DIR ]
+	then
+		cp -f $PWD/rd-autostart.desktop $AUTOSTART_DIR/
+		echo "Exec=${RD_BIN_PATH} -daemon" >> $AUTOSTART_DIR/rd-autostart.desktop
+	fi
+fi
 
 # Setup Bash integration
 CDR_SCRIPT=$PWD/rd.bash
-echo "source \"$CDR_SCRIPT\"" >> ~/.bashrc
+
+if [ $OSX ]
+then
+	SHELL_INIT_FILE=~/.bash_profile
+else
+	SHELL_INIT_FILE=~/.bashrc
+fi
+
+sed -i .bak "/rd.bash/d" $SHELL_INIT_FILE
+echo "source \"$CDR_SCRIPT\"" >> $SHELL_INIT_FILE
 
 # Start the rd daemon
-killall -q rd
+if [ $OSX ]
+then
+	killall rd 2>/dev/null
+else
+	killall -q rd
+fi
+
 nohup rd -daemon 2>/dev/null &
 
 echo "Setup complete. Start a new shell to use the 'cdr' command"
